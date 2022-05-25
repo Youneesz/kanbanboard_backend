3abb7a896b4f6c991bdc5243672f047164fa4c9b
Youneesz <94466415+Youneesz@users.noreply.github.com>
2022-05-25 01:19:52 +0100

    Update LoginServiceImpl.java

--- src/main/java/com/pfeproject/kanbanboard/service/LoginServiceImpl.java
+++ src/main/java/com/pfeproject/kanbanboard/service/LoginServiceImpl.java
@@ -1,42 +1,42 @@
-package com.pfeproject.kanbanboard.service;
+    package com.pfeproject.kanbanboard.service;
 
-import com.pfeproject.kanbanboard.model.Utilisateur;
-import com.pfeproject.kanbanboard.repository.UtilisateurRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
-import org.springframework.stereotype.Service;
+    import com.pfeproject.kanbanboard.model.Utilisateur;
+    import com.pfeproject.kanbanboard.repository.UtilisateurRepository;
+    import org.springframework.beans.factory.annotation.Autowired;
+    import org.springframework.mail.SimpleMailMessage;
+    import org.springframework.mail.javamail.JavaMailSender;
+    import org.springframework.mail.javamail.JavaMailSenderImpl;
+    import org.springframework.stereotype.Service;
 
-@Service
-public class LoginServiceImpl implements LoginService{
+    @Service
+    public class LoginServiceImpl implements LoginService{
 
-    @Autowired
-    private final UtilisateurRepository utilisateurRepository;
-    @Autowired
-    private JavaMailSender mailSender;
+        @Autowired
+        private final UtilisateurRepository utilisateurRepository;
+        @Autowired
+        private JavaMailSender mailSender;
 
-    public LoginServiceImpl(UtilisateurRepository utilisateurRepository) {
-        this.utilisateurRepository = utilisateurRepository;
-    }
-
-    @Override
-    public Utilisateur userLogin(String username, String password) {
-        return utilisateurRepository.getUserLogin(username, password);
-    }
-
-    @Override
-    public String resetPassEmail(String toEmail, String subject, String body) {
-        if (utilisateurRepository.findAll().stream().anyMatch(e -> e.getEmail().equals(toEmail))) {
-            SimpleMailMessage msg = new SimpleMailMessage();
-            msg.setFrom("kanbanboardreset@gmail.com");
-            msg.setTo(toEmail);
-            msg.setText(body);
-            msg.setSubject(subject);
-
-            mailSender.send(msg);
-            return "Check your email messages to reset your password.";
+        public LoginServiceImpl(UtilisateurRepository utilisateurRepository) {
+            this.utilisateurRepository = utilisateurRepository;
         }
-        return "There isn't a user with this email.";
+
+        @Override
+        public Utilisateur userLogin(String username, String password) {
+            return utilisateurRepository.getUserLogin(username, password);
+        }
+
+        @Override
+        public String resetPassEmail(String toEmail, String subject, String body) {
+            if (utilisateurRepository.findAll().stream().anyMatch(e -> e.getEmail().equals(toEmail))) {
+                SimpleMailMessage msg = new SimpleMailMessage();
+                msg.setFrom("kanbanboardreset@gmail.com");
+                msg.setTo(toEmail);
+                msg.setText(body);
+                msg.setSubject(subject);
+
+                mailSender.send(msg);
+                return "Check your email messages to reset your password.";
+            }
+            return "There isn't a user with this email.";
+        }
     }
-}
